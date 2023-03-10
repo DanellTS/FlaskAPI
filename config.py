b9f3b63228aa81af184b98c1256e7faf6fbd2ef3
DanellTS <danell.ts@gmail.com>
2023-03-09 22:35:11 -0400

    .

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import openai
 
 class DevelopmentConfig():
         DEBUG = True
-        openai.api_key = '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'
+        openai.api_key = 'YOUR-APIKEY-HERE'
     
 config = {
     'development': DevelopmentConfig
